87b2612bb5017e6bc8ae9170e21465fb3719995d
jcoonrod <54367582+jcoonrod@users.noreply.github.com>
2021-07-25 15:54:10 -0400

    Add files via upload

---
+++ go
@@ -0,0 +1,1 @@
+./../.deno/bin/deno run --allow-net --allow-read --unstable server.ts
---
+++ server.ts
@@ -0,0 +1,25 @@
+// Simple webserver with static directory for css and image files
+async function handle(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    const url = new URL(requestEvent.request.url);
+    const p=url.pathname;
+    const mime=String(mimetypes[p.substr(p.lastIndexOf('.')+1)]);
+    console.log(`path: ${p}`);
+    // do we serve a file or generate a dynamic page?
+    if(p=='/favicon.ico' || p=='/robots.txt' || p.substr(0,7)=='/static') {
+      const buf=await Deno.readFile("."+p);
+      await requestEvent.respondWith(
+        new Response(buf,{status:200, headers:{"Content-type":mime}})
+      );
+    }else{
+      await requestEvent.respondWith(
+        new Response(hello, {status: 200, headers:{"Content-type":"text/html" } })
+      );  
+    } 
+  }
+}
+const mimetypes: Record<string,string>={'svg':'image/svg+xml','jpg':'image/jpeg','png':'image/png','txt':'text/plain','css':'text/css','jpeg':'image/jpeg'};
+const hello=await Deno.readFile('./template.html');
+const server = Deno.listen({ port: 8080 });
+for await (const conn of server) {handle(conn);}
